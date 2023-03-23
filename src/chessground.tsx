652e6bc838bcd23dc8bab8cf386ab5c505ca0029
Ben Martin <1350682+ironstrider@users.noreply.github.com>
2023-03-24 08:22:06 +1000

    Add jsx component

--- src/chessground.tsx
+++ src/chessground.tsx
@@ -1,1 +1,33 @@
-// TODO: add simple component
+import { Chessground as Cg } from "chessground";
+import { Api } from "chessground/api";
+import { Config } from "chessground/config";
+import { createEffect, on, onCleanup } from "solid-js";
+
+interface ChessgroundProps {
+  config: Partial<Config>,
+  style?: string,
+};
+
+export default function Chessground(props: ChessgroundProps) {
+  let api: Api | null = null;
+
+  let mount = (el: HTMLDivElement) => {
+    api = Cg(el, props.config);
+  }
+
+  createEffect(on(
+    () => props.config,
+    (config) => {
+      api?.set(config)
+    },
+    {defer: true}
+  ));
+
+  onCleanup(() => {
+    api?.destroy();
+  });
+
+  return (
+    <div style={props.style || "width: 100%; aspect-ratio: 1/1"} ref={el => mount(el)}></div>
+  )
+}
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 export { useChessground } from "./use-chessground";
 export type { Config, ChessgroundState } from "./use-chessground";
+
+import Chessground from "./chessground";
+export { Chessground }
